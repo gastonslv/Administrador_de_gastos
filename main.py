6e8ea67f196419c1f99b717f46df1cbde4a4bf38
Gaston Silvestre <gastonsilvestrebrc@gmail.com>
2023-10-03 09:45:38 -0300

    creamos las listas donde vamos a guardar los datos

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 """"
 Gestor de gastos
 """
-
+import json
 from funciones.f_menu import menu_principal as menu
+from funciones.f_cargar_datos import * #json cargar_datos guardar_datos
 
-# Variables
+## Variables
 salir_programa = False
 d_usuario_menu = 0 # futura decision del usuario
+
+## Cargamos
+
+gastos = []
+
+ingresos = []
+
+ahorros = []
+
+fondos = []
 
 print("Bienvenido al gestor de gastos!")
 
 while salir_programa == False:
     d_usuario_menu = menu()
-    print(d_usuario_menu)
+    
 
