381e68f522752999bb48ced357f4dda2c203ba11
Gaston Silvestre <gastonsilvestrebrc@gmail.com>
2023-10-04 17:39:37 -0300

    agrego la funcion de c_ahorro a la funcion calcular

--- funciones/f_calcular.py
+++ funciones/f_calcular.py
@@ -1,11 +1,13 @@
 from funciones.f_operaciones import calcular_gastos as  c_gastos
 from funciones.f_operaciones import calcular_ingresos as  c_ingresos
-
+from funciones.f_operaciones import calcular_ahorro as c_ahorro
 def calcular(operacion, fondos, ahorros, gastos, ingresos, movimientos):
     if operacion == 1:
         c_gastos(fondos, gastos, movimientos)
     elif operacion == 2:
         c_ingresos(fondos, ingresos, movimientos)
+    elif operacion == 3:
+        c_ahorro(fondos, ahorros, movimientos)
     return 0
 
 
