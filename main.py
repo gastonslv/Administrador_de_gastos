d5dc7c616707e63ee6fb94473402fc0f1c2f1faa
Gaston Silvestre <gastonsilvestrebrc@gmail.com>
2023-10-04 13:15:21 -0300

    cambio de variable

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ Gestor de gastos
 """
 import json
 from funciones.f_menu import menu_principal as menu
+from funciones.f_calcular import calcular
 from funciones.f_cargar_datos import * #json cargar_datos guardar_datos
 
 ## caja 1
