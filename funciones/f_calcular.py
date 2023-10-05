ab0aaa4c3bf6791d09db1f3495abafbadbb7a335
Gaston Silvestre <gastonsilvestrebrc@gmail.com>
2023-10-05 13:26:22 -0300

    Agregamos la operacion 4, relacionada con movimientos

--- funciones/f_calcular.py
+++ funciones/f_calcular.py
@@ -1,6 +1,8 @@
 from funciones.f_operaciones import calcular_gastos as  c_gastos
 from funciones.f_operaciones import calcular_ingresos as  c_ingresos
 from funciones.f_operaciones import calcular_ahorro as c_ahorro
+from funciones.f_operaciones import mostrar_movimientos
+
 def calcular(operacion, fondos, ahorros, gastos, ingresos, movimientos):
     if operacion == 1:
         c_gastos(fondos, gastos, movimientos)
@@ -8,6 +10,8 @@ def calcular(operacion, fondos, ahorros, gastos, ingresos, movimientos):
         c_ingresos(fondos, ingresos, movimientos)
     elif operacion == 3:
         c_ahorro(fondos, ahorros, movimientos)
+    elif operacion == 4:
+        mostrar_movimientos(movimientos)
     return 0
 
 
